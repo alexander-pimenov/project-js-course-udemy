7d0516cbd3362834694de5b9b7fa0e8d0287e115
Alex <pimalex1978@yandex.ru>
2022-12-11 19:21:09 +0300

    урок с ветвлениями,тернарный оператор

--- js/script5.js
+++ js/script5.js
@@ -1,9 +1,51 @@
 "use strict";
 
-// 1
 // Работа с условиями
 
+const num = 50;
 
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Ок!');
+}
 
+//тернарный оператор - в нем учавствуют три аргумента
+(num === 50) ? console.log('Ок!') : console.log('Error');
 
+//switch-case - идет всегда на строго сравнение
+//с числами
+const numm = 51;
+switch (numm) {
+    case 49:
+        console.log('Неверно!');
+        break;
+    case 100:
+        console.log('Неверно!');
+        break;
+    case 50:
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз!');
+        break;
+}
 
+//или со строками
+const str = '51';
+switch (str) {
+    case '49':
+        console.log('Неверно!');
+        break;
+    case '100':
+        console.log('Неверно!');
+        break;
+    case '50':
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз!');
+        break;
+}
